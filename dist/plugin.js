fa87e3670546e3a01de46f93f28f1fb11a3284e8
Massimo Melina <a@rejetto.com>
2025-10-24 20:27:55 +0200

    fix: dupe icon when used together with "thumbnails" plugin

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,5 +1,5 @@
-exports.repo = "rejetto/click-icon-to-show"
-exports.version = 1
+exports.repo = "rejetto/hfs-click-icon-to-show"
+exports.version = 1.1
 exports.description = "Clicking an icon will be like clicking \"show\" in the menu"
-exports.apiRequired = 12.5 // fileShow returning boolean
+exports.apiRequired = 12.94 // onEvent:after
 exports.frontend_js = "main.js"
--- dist/public/main.js
+++ dist/public/main.js
@@ -1,4 +1,4 @@
-HFS.onEvent('entryIcon', ({ def, entry }, { output }) => {
+HFS.onEvent('entryIcon:after', ({ def, entry }, { output }) => {
     def = output.find(Boolean) || def // use other plugin's result, if any, as new default
     output.length = 0 // avoid showing multiple icons
     return HFS.h('span', { // wrap with an element
